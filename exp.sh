1c2bbeb1dceea099cd04442f3ec47d1244140ae9
John Jiang <john.xiajun.jiang@gmail.com>
2024-03-09 19:54:50 -0500

    Update environment

--- exp.sh
+++ exp.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
+# ST-GCNN Heart to Heart
 python main.py --config demo01 --stage 1
-python main.py --config demo01 --stage 2 --eval eval
+python main.py --config demo01 --stage 2 --tag qry0,qry1
+
+# Euclidean (Sandesh et al, IPMI 2019)
+python main.py --config demo02 --stage 1
+python main.py --config demo02 --stage 2 --tag qry0,qry1
+
+# Meta-model with GRU dynamics (Jiang et al, ICLR 2023)
+python main.py --config demo03 --stage 1
+python main.py --config demo03 --stage 2 --tag qry0,qry1
+
+# ST-GCNN Inverse problem (Jiang et al, IEEE TMI 2022/MICCAI 2020)
+python main.py --config demo04 --stage 1
+python main.py --config demo04 --stage 2 --tag qry0,qry1
+
+# Meta-model with Neural ODE dynamics
+python main.py --config demo05 --stage 1
+python main.py --config demo05 --stage 2 --tag qry0,qry1
+
+# Meta-model with GRU dynamics (Jiang et al, MICCAI 2022)
+python main.py --config demo06 --stage 1
+python main.py --config demo06 --stage 2 --tag qry0,qry1
--- run_pip_req.sh
+++ run_pip_req.sh
@@ -4,4 +4,4 @@ CUDA=cu117
 
 pip install -r requirements.txt
 
-pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+${CUDA}.html
+pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-2.0.1+${CUDA}.html
