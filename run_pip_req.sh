b2ebb76413a263ca36749ff506040bf58e465513
John Jiang <john.xiajun.jiang@gmail.com>
2024-04-16 20:46:16 -0400

    update setups

--- run_pip_req.sh
+++ run_pip_req.sh
@@ -4,4 +4,6 @@ CUDA=cu117
 
 pip install -r requirements.txt
 
+pip install torch==2.0.1
+
 pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-2.0.1+${CUDA}.html
